0109adb80e0d10066d6af03b325dac1fb011ef9d
MertalBahar <mertalbahar@windowslive.com>
2023-12-27 22:19:05 +0300

    Business Layer created, Products listed.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using Nothwind.DataAccess.Concrete;
+using Nothwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
