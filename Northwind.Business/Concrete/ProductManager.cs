206acabf9cf176c19092d9df188665ef62d4c70f
MertalBahar <mertalbahar@windowslive.com>
2023-12-27 23:11:31 +0300

    Refactoring code smell. 1. ProductDal named EfProductDal. 2. IProductDal created. 3. Form1 updated.

--- Northwind.Business/Concrete/ProductManager.cs
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
-﻿using Nothwind.DataAccess.Concrete;
+﻿using Nothwind.DataAccess.Abstract;
+using Nothwind.DataAccess.Concrete.EntityFramework;
 using Nothwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,13 @@ namespace Northwind.Business.Concrete
 {
     public class ProductManager
     {
-        ProductDal _productDal = new ProductDal();
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
